b76ddfb9c1445fd99b763496a04dd90f809315fd
andrewpak <andrew7pak@gmail.com>
2022-05-31 01:04:04 -0400

    fixed spacing

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const server = http.createServer();
 let task_states = [];
 let task;
 let mealOne;
+
 server.on("request", connection_handler);
 
 function connection_handler(req, res){
@@ -54,35 +55,35 @@ function redirect_to_calendar(state, res){
 }
 
 function spoonacular_query(ingredient, res){
-console.log(ingredient);
-let ing = ingredient.ingredients;
-console.log(ing);
-const spoonacular_endpoint = "https://api.spoonacular.com/recipes/findByIngredients";
-let uri = queryString.stringify({apiKey, limitLicense, ignorePantry});
-console.log(apiKey);
-let newEndpoint =`${spoonacular_endpoint}?${uri}&ingredients=${ing}`;
-https.request(newEndpoint, (task_stream) => process_stream(task_stream, reveive_spoonacular_response, res)).end();
+  console.log(ingredient);
+  let ing = ingredient.ingredients;
+  console.log(ing);
+  const spoonacular_endpoint = "https://api.spoonacular.com/recipes/findByIngredients";
+  let uri = queryString.stringify({apiKey, limitLicense, ignorePantry});
+  console.log(apiKey);
+  let newEndpoint =`${spoonacular_endpoint}?${uri}&ingredients=${ing}`;
+  https.request(newEndpoint, (task_stream) => process_stream(task_stream, reveive_spoonacular_response, res)).end();
 }
 
 function send_calendar_task(task, user_token, res){
-const task_endpoint = "https://www.googleapis.com/calendar/v3/calendars/222ccm0g98bm50knud8rll62co@group.calendar.google.com/events/quickAdd";
-const post_data = JSON.stringify({"text":task});
-token_request_time = new Date(); // for cache
-console.log(post_data);
-console.log(post_data);
+  const task_endpoint = "https://www.googleapis.com/calendar/v3/calendars/222ccm0g98bm50knud8rll62co@group.calendar.google.com/events/quickAdd";
+  const post_data = JSON.stringify({"text":task});
+  token_request_time = new Date(); // for cache
+  console.log(post_data);
+  console.log(post_data);
 
-let myToken =  user_token.access_token;
-let myScope = user_token.scope;
-      const options = {
-            method: "POST",
-            headers: {
-              Authorization: `Bearer ${myToken}`,
-              scope: myScope
-                }
-            }
-https.request(task_endpoint, options, (task_stream) => process_stream(task_stream, receive_task_response, res)).end(post_data);
-let formStream =  fs.createReadStream("./html/success.html");
-formStream.pipe(res);
+  let myToken =  user_token.access_token;
+  let myScope = user_token.scope;
+        const options = {
+              method: "POST",
+              headers: {
+                Authorization: `Bearer ${myToken}`,
+                scope: myScope
+                  }
+              }
+  https.request(task_endpoint, options, (task_stream) => process_stream(task_stream, receive_task_response, res)).end(post_data);
+  let formStream =  fs.createReadStream("./html/success.html");
+  formStream.pipe(res);
 }
 
 
@@ -93,49 +94,50 @@ function process_stream (stream, callback , ...args){
 }
 
 function receive_task_response(body, res){
-const results = JSON.parse(body);
-console.log(results);
-res.writeHead(302, {Location:`${results.url}`}).end();
+  const results = JSON.parse(body);
+  console.log(results);
+  res.writeHead(302, {Location:`${results.url}`}).end();
 }
 
 function reveive_spoonacular_response(body, res){
-const results = JSON.parse(body);
-let randomInt = getRandomInt(10);
-if(Object.keys(results).length === 0){ //if the users input didn't yield any results from the API
-    not_found(res);
-} else {
-mealOne = results[randomInt].title; // selects a random meal title relative to the user input from the json that the api sent back
-console.log(mealOne);
-//console.log(results);
+  const results = JSON.parse(body);
+  let randomInt = getRandomInt(10);
+  if(Object.keys(results).length === 0){ //if the users input didn't yield any results from the API
+      not_found(res);
+  } else {
+  mealOne = results[randomInt].title; // selects a random meal title relative to the user input from the json that the api sent back
+  console.log(mealOne);
+  //console.log(results);
 
-task = mealOne;
-const state = crypto.randomBytes(20).toString("hex"); // creating session
-task_states.push({task, state});
+  task = mealOne;
+  const state = crypto.randomBytes(20).toString("hex"); // creating session
+  task_states.push({task, state});
 
-const token_cache = './auth/cache.json';
-let cache_valid = false;
-    if(fs.existsSync(token_cache)){
-    cache_token = require(token_cache);
-    if(new Date(cache_token.expiration) > Date.now()){
-        cache_valid = true;
-        }
-    }
-    if(cache_valid){
-    let access_token = cache_token;
-    console.log("Cache Exists and is Valid")
-    //synchronously sends google calendar the meal name and adds it as an event
-    //if the token has already been cached
-    send_calendar_task(mealOne, access_token, res);
-    } else {
-    //synchronously calls the other API , google calendar.    
-    redirect_to_calendar(state,res);
-    }
- }
+  const token_cache = './auth/cache.json';
+  let cache_valid = false;
+      if(fs.existsSync(token_cache)){
+      cache_token = require(token_cache);
+      if(new Date(cache_token.expiration) > Date.now()){
+          cache_valid = true;
+          }
+      }
+      if(cache_valid){
+      let access_token = cache_token;
+      console.log("Cache Exists and is Valid")
+      //synchronously sends google calendar the meal name and adds it as an event
+      //if the token has already been cached
+      send_calendar_task(mealOne, access_token, res);
+      } else {
+      //synchronously calls the other API , google calendar.    
+      redirect_to_calendar(state,res);
+      }
+   }
 }
 // needed this function to generate random int
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
+
 function create_token_cache (token, token_request_time){
     token.expiration = new Date(token_request_time.getTime() + (token.expires_in * 1000)) ;
     console.log("Token Object", token);
@@ -146,6 +148,7 @@ function not_found(res){
     res.writeHead(404, {"Content-Type":"text/plain"});
     res.write("404 Not Found",()=>res.end());
 }
+
 server.on("listening", () => console.log(`Now Listening on Port ${port}`));
 server.listen(port);
 
